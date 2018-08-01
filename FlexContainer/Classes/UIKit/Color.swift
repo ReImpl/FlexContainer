31e4a8ee422fb2e0c654d45f1fc5ff1de64a203f
kernel <kernel@reimplement.mobi>
2018-07-31 21:49:49 -0500

    Fixed UIColor.random.

--- FlexContainer/Classes/UIKit/Color.swift
+++ FlexContainer/Classes/UIKit/Color.swift
@@ -19,9 +19,9 @@ public extension UIColor {
 	}
 	
 	static var random: UIColor {
-		let r = CGFloat.random(in: 0...255)
-		let g = CGFloat.random(in: 0...255)
-		let b = CGFloat.random(in: 0...255)
+		let r = CGFloat.random(in: 0...1)
+		let g = CGFloat.random(in: 0...1)
+		let b = CGFloat.random(in: 0...1)
 		
 		return UIColor(red: r, green: g, blue: b, alpha: 1)
 	}
